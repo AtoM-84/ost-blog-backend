f1eae429c79fd6e21d8ca6b4fb4618ded036a7cb
louise1991 <lelivrelouise@gmail.com>
2024-08-01 16:03:00 +0200

    test router for 404 status commited with louise1991 account

--- app/routers/router.js
+++ app/routers/router.js
@@ -5,8 +5,16 @@ router.get('/',(req, res, next) => {
         res.send(`<h1>hello world!</h1>`)
 })
 
-router.get('/:postId',(req, res) => {
-        res.send(`<h1>Tu veux l'article ${req.params.postId}</h1>`)
+router.get('/blog/:postId',(req, res) => {
+
+        if (req.params.postId <= 10 ) {
+                res.send(`<h1>Tu veux l'article ${req.params.postId}</h1>`)
+        }
+        else res.status(404).send("Je n'ai pas trouvé cet article")
 })
+
+router.use((req, res, next) => {
+        res.status(404).json({error: 'Page not found.'});
+      });
 
 export default router;  
