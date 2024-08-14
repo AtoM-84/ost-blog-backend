e7150e25a80d8c8bcbf53222f014748de009853d
louise1991 <lelivrelouise@gmail.com>
2024-08-14 17:05:03 +0200

    new seeding

--- data/populate_tables_post-author.sql
+++ data/populate_tables_post-author.sql
@@ -1,7 +1,16 @@
 BEGIN;
 
+-- 1 post --
 INSERT INTO "post" ("title", "sub_title", "created_at", "updated_at", "content") VALUES
-('Advances in Topology research for AI training', 'The predictability of trafic patterns allows for cost effective and optimized network topologies', '20240721', '20240729', '### Titre 3
+('Advances in Topology research with AI training', 'Predictability of trafic patterns allows for cost effective and optimized network topologies', '20240721', '20240729', 
+
+'## Titre 2
+
+#### Subtitle
+
+##### Author
+
+###### Publication date
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
 
@@ -14,14 +23,80 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendi
 *   item 3
 *   item 4
 
-uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien](https://dev.to/devluc/50-best-websites-for-web-design-inspiration-and-ideas-be6) uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien b](https://dev.to/devluc/50-best-websites-for-web-design-inspiration-and-ideas-be6)
+uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien](https://dev.to/copilotkit/how-i-upped-my-frontend-game-with-generative-ui-4fhc) uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien b]https://dev.to/copilotkit/how-i-upped-my-frontend-game-with-generative-ui-4fhc)
 
-[lien c](https://dev.to/devluc/50-best-websites-for-web-design-inspiration-and-ideas-be6)');
+[lien c](https://dev.to/copilotkit/how-i-upped-my-frontend-game-with-generative-ui-4fhc)');
 
+-- 1 author --
 INSERT INTO "author" ("name", "email") VALUES
-('Atom84', 'aphelipot@gmail.com');
+('atom84', 'arnaudphelipot@gmail.com');
 
 -- INSERT INTO "comment" ("created_at", "content", "post_id", "author_id") VALUES
 -- ('20240731', 'Amazing article, thanks for sharing !', '0', '0');
+
+
+-- 2 post --
+INSERT INTO "post" ("title", "sub_title", "created_at", "updated_at", "content") VALUES
+('How to create a join query with Knex', 'Retrieving data from two join tables in a REST API', '20200302', '20200622', 
+'## Titre 2
+
+#### Subtitle
+
+##### Author
+
+###### Publication date
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+![picture](http://url.com)
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. _Duis semper. Duis arcu massa_Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+*   item 1
+*   item 2
+*   item 3
+*   item 4
+
+uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien](https://dev.to/cesareferrari/how-to-create-an-api-endpoint-query-with-knex-360h) uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien b](https://dev.to/cesareferrari/how-to-create-an-api-endpoint-query-with-knex-360h)
+
+[lien c](https://dev.to/cesareferrari/how-to-create-an-api-endpoint-query-with-knex-360h)');
+
+
+-- 2 author --
+INSERT INTO "author" ("name", "email") VALUES
+('Lorenzo', 'lorenzoferrari@dev.com');
+
+
+-- 3 post --
+INSERT INTO "post" ("title", "sub_title", "created_at", "updated_at", "content") VALUES
+('React Redux Toolkit', 'Problem Statement', '20240627', '20240701', 
+
+'## Titre 2
+
+#### Subtitle
+
+##### Author
+
+###### Publication date
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+![picture](http://url.com)
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. _Duis semper. Duis arcu massa_Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+
+*   item 1
+*   item 2
+*   item 3
+*   item 4
+
+uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien](https://dev.to/dharamgfx/redux-toolkit-vs-react-context-api-a-deep-dive-into-state-management-2b2n) uis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; [lien b](https://dev.to/dharamgfx/redux-toolkit-vs-react-context-api-a-deep-dive-into-state-management-2b2n)
+
+[lien c](https://dev.to/dharamgfx/redux-toolkit-vs-react-context-api-a-deep-dive-into-state-management-2b2n)');
+
+
+-- 3 author --
+INSERT INTO "author" ("name", "email") VALUES
+('Julian Moore', 'jumoore@dev.com');
 
 COMMIT;
