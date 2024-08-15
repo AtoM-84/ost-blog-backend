c7c82757db9a51ec92ffbf2eade6124958a2a9f2
louise1991 <lelivrelouise@gmail.com>
2024-08-15 18:57:13 +0200

    route to middleware error handler and controller added for blog list

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,20 +1,24 @@
 import { Router } from "express";
+import controllerHandler from '../middlewares/controller.handler.js';
+import blogController from "../controllers/blogController.js";
 
-const router = Router(); 
-router.get('/',(req, res, next) => {
+const router = Router();
+
+
+router.get('/', (req, res, next) => {
         res.send(`<h1>hello world!</h1>`)
 })
 
-router.get('/blog/:postId',(req, res) => {
-
-        if (req.params.postId <= 10 ) {
-                res.send(`<h1>Tu veux l'article ${req.params.postId}</h1>`)
-        }
-        else res.status(404).send("Je n'ai pas trouvé cet article")
-})
+router.get('/blog/', controllerHandler(blogController.blogList))
+// router.get('/blog/:blogPage', controllerHandler(blogController.blogList))
+// router.get('/blog/asc/:blogPage', controllerHandler(blogController.blogList)) 
+// router.get('/blog/author/:author/:blogPage', controllerHandler()) // use getByAuthor in datamapper
+// router.get('/blog/item/:postId', controllerHandler(blogController.blogItem)) // use findByKey in datamapper
 
 router.use((req, res, next) => {
-        res.status(404).json({error: 'Page not found.'});
-      });
+        res.status(404).json({ error: 'Page not found.' });
+});
 
-export default router;  
+// router.use(errorHandler);
+
+export default router;
