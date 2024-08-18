843a7fb2f8329cd50556626f7e4c0ba572ca1990
louise1991 <lelivrelouise@gmail.com>
2024-08-18 15:47:43 +0200

    add new cors policy to test on local machine

--- app/index.app.js
+++ app/index.app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import router from "./routers/router.js";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
+  
 app.use(router);
 export default app;
