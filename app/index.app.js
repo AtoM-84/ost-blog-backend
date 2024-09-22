6f0dfb335ab3fe227b9d4febb8c90925f2e1701b
louise1991 <lelivrelouise@gmail.com>
2024-09-22 21:03:24 +0200

    change cors

--- app/index.app.js
+++ app/index.app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://ost-static-react.railway.internal',
     credentials: true,
   }));
 app.use(router);
